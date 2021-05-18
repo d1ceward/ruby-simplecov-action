c971bcc1357124249d7a29a07dff87954a93a3da
Fabien LEFEBVRE <contact@d1ceward.com>
2021-05-19 01:40:59 +0200

    Add comment creation

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import * as core from '@actions/core'
+import * as github from '@actions/github'
 
 async function run(): Promise<void> {
   try {
-    console.log("pouet")
+    const pullRequestId = github.context.issue.number
+    if (!pullRequestId) {
+      throw new Error('Cannot find the PR id')
+    }
+
+    const octokit = github.getOctokit(core.getInput('token'))
+
+    await octokit.rest.issues.createComment({
+      owner: github.context.repo.owner,
+      repo: github.context.repo.repo,
+      issue_number: pullRequestId,
+      body: 'Test'
+    })
   }
   catch (error) {
     core.setFailed(error.message)

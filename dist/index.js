f93a5f53640df8a1c5e20071068d5470c7eb2af3
Fabien LEFEBVRE <contact@d1ceward.com>
2021-05-31 03:42:26 +0200

    Add basic summary

--- dist/index.js
+++ dist/index.js
@@ -94,11 +94,20 @@ const path = __importStar(__nccwpck_require__(622));
 const fs = __importStar(__nccwpck_require__(747));
 const core = __importStar(__nccwpck_require__(186));
 const github = __importStar(__nccwpck_require__(438));
+const markdown_table_1 = __nccwpck_require__(62);
 const comment_1 = __importDefault(__nccwpck_require__(667));
 const WORKSPACE = process.env.GITHUB_WORKSPACE;
 function parseLastRun(filePath) {
     const content = fs.readFileSync(path.resolve(WORKSPACE, filePath));
     return JSON.parse(content.toString());
+}
+function summaryTable() {
+    const baseCoveredPercent = parseLastRun('base-coverage-reports/.last_run.json');
+    const headCoveredPercent = parseLastRun('head-coverage-reports/.last_run.json');
+    return markdown_table_1.markdownTable([
+        ['Base Coverage', 'Head Coverage'],
+        [`${baseCoveredPercent.result.line}%`, `${headCoveredPercent.result.line}%`]
+    ]);
 }
 async function run() {
     try {
@@ -107,8 +116,7 @@ async function run() {
             core.warning('Cannot find the PR id');
             return;
         }
-        const coveredPercent = parseLastRun('head-coverage-reports/.last_run.json');
-        await comment_1.default(pullRequestId, `Test coverage percent: ${coveredPercent}`);
+        await comment_1.default(pullRequestId, summaryTable());
     }
     catch (error) {
         core.setFailed(error.message);
@@ -3970,6 +3978,279 @@ exports.isPlainObject = isPlainObject;
 
 /***/ }),
 
+/***/ 62:
+/***/ ((__unused_webpack___webpack_module__, __webpack_exports__, __nccwpck_require__) => {
+
+"use strict";
+__nccwpck_require__.r(__webpack_exports__);
+/* harmony export */ __nccwpck_require__.d(__webpack_exports__, {
+/* harmony export */   "markdownTable": () => (/* binding */ markdownTable)
+/* harmony export */ });
+/* harmony import */ var repeat_string__WEBPACK_IMPORTED_MODULE_0__ = __nccwpck_require__(976);
+
+
+/**
+ * @typedef MarkdownTableOptions
+ * @property {string|string[]} [align]
+ * @property {boolean} [padding=true]
+ * @property {boolean} [delimiterStart=true]
+ * @property {boolean} [delimiterStart=true]
+ * @property {boolean} [delimiterEnd=true]
+ * @property {boolean} [alignDelimiters=true]
+ * @property {(value: string) => number} [stringLength]
+ */
+
+/**
+ * Create a table from a matrix of strings.
+ *
+ * @param {string[][]} table
+ * @param {MarkdownTableOptions} [options]
+ * @returns {string}
+ */
+function markdownTable(table, options) {
+  var settings = options || {}
+  var align = (settings.align || []).concat()
+  var stringLength = settings.stringLength || defaultStringLength
+  /** @type {number[]} Character codes as symbols for alignment per column. */
+  var alignments = []
+  var rowIndex = -1
+  /** @type {string[][]} Cells per row. */
+  var cellMatrix = []
+  /** @type {number[][]} Sizes of each cell per row. */
+  var sizeMatrix = []
+  /** @type {number[]} */
+  var longestCellByColumn = []
+  var mostCellsPerRow = 0
+  /** @type {number} */
+  var columnIndex
+  /** @type {string[]} Cells of current row */
+  var row
+  /** @type {number[]} Sizes of current row */
+  var sizes
+  /** @type {number} Sizes of current cell */
+  var size
+  /** @type {string} Current cell */
+  var cell
+  /** @type {string[]} */
+  var lines
+  /** @type {string[]} Chunks of current line. */
+  var line
+  /** @type {string} */
+  var before
+  /** @type {string} */
+  var after
+  /** @type {number} */
+  var code
+
+  // This is a superfluous loop if we don’t align delimiters, but otherwise we’d
+  // do superfluous work when aligning, so optimize for aligning.
+  while (++rowIndex < table.length) {
+    columnIndex = -1
+    row = []
+    sizes = []
+
+    if (table[rowIndex].length > mostCellsPerRow) {
+      mostCellsPerRow = table[rowIndex].length
+    }
+
+    while (++columnIndex < table[rowIndex].length) {
+      cell = serialize(table[rowIndex][columnIndex])
+
+      if (settings.alignDelimiters !== false) {
+        size = stringLength(cell)
+        sizes[columnIndex] = size
+
+        if (
+          longestCellByColumn[columnIndex] === undefined ||
+          size > longestCellByColumn[columnIndex]
+        ) {
+          longestCellByColumn[columnIndex] = size
+        }
+      }
+
+      row.push(cell)
+    }
+
+    cellMatrix[rowIndex] = row
+    sizeMatrix[rowIndex] = sizes
+  }
+
+  // Figure out which alignments to use.
+  columnIndex = -1
+
+  if (typeof align === 'object' && 'length' in align) {
+    while (++columnIndex < mostCellsPerRow) {
+      alignments[columnIndex] = toAlignment(align[columnIndex])
+    }
+  } else {
+    code = toAlignment(align)
+
+    while (++columnIndex < mostCellsPerRow) {
+      alignments[columnIndex] = code
+    }
+  }
+
+  // Inject the alignment row.
+  columnIndex = -1
+  row = []
+  sizes = []
+
+  while (++columnIndex < mostCellsPerRow) {
+    code = alignments[columnIndex]
+    before = ''
+    after = ''
+
+    if (code === 99 /* `c` */) {
+      before = ':'
+      after = ':'
+    } else if (code === 108 /* `l` */) {
+      before = ':'
+    } else if (code === 114 /* `r` */) {
+      after = ':'
+    }
+
+    // There *must* be at least one hyphen-minus in each alignment cell.
+    size =
+      settings.alignDelimiters === false
+        ? 1
+        : Math.max(
+            1,
+            longestCellByColumn[columnIndex] - before.length - after.length
+          )
+
+    cell = before + repeat_string__WEBPACK_IMPORTED_MODULE_0__('-', size) + after
+
+    if (settings.alignDelimiters !== false) {
+      size = before.length + size + after.length
+
+      if (size > longestCellByColumn[columnIndex]) {
+        longestCellByColumn[columnIndex] = size
+      }
+
+      sizes[columnIndex] = size
+    }
+
+    row[columnIndex] = cell
+  }
+
+  // Inject the alignment row.
+  cellMatrix.splice(1, 0, row)
+  sizeMatrix.splice(1, 0, sizes)
+
+  rowIndex = -1
+  lines = []
+
+  while (++rowIndex < cellMatrix.length) {
+    row = cellMatrix[rowIndex]
+    sizes = sizeMatrix[rowIndex]
+    columnIndex = -1
+    line = []
+
+    while (++columnIndex < mostCellsPerRow) {
+      cell = row[columnIndex] || ''
+      before = ''
+      after = ''
+
+      if (settings.alignDelimiters !== false) {
+        size = longestCellByColumn[columnIndex] - (sizes[columnIndex] || 0)
+        code = alignments[columnIndex]
+
+        if (code === 114 /* `r` */) {
+          before = repeat_string__WEBPACK_IMPORTED_MODULE_0__(' ', size)
+        } else if (code === 99 /* `c` */) {
+          if (size % 2) {
+            before = repeat_string__WEBPACK_IMPORTED_MODULE_0__(' ', size / 2 + 0.5)
+            after = repeat_string__WEBPACK_IMPORTED_MODULE_0__(' ', size / 2 - 0.5)
+          } else {
+            before = repeat_string__WEBPACK_IMPORTED_MODULE_0__(' ', size / 2)
+            after = before
+          }
+        } else {
+          after = repeat_string__WEBPACK_IMPORTED_MODULE_0__(' ', size)
+        }
+      }
+
+      if (settings.delimiterStart !== false && !columnIndex) {
+        line.push('|')
+      }
+
+      if (
+        settings.padding !== false &&
+        // Don’t add the opening space if we’re not aligning and the cell is
+        // empty: there will be a closing space.
+        !(settings.alignDelimiters === false && cell === '') &&
+        (settings.delimiterStart !== false || columnIndex)
+      ) {
+        line.push(' ')
+      }
+
+      if (settings.alignDelimiters !== false) {
+        line.push(before)
+      }
+
+      line.push(cell)
+
+      if (settings.alignDelimiters !== false) {
+        line.push(after)
+      }
+
+      if (settings.padding !== false) {
+        line.push(' ')
+      }
+
+      if (
+        settings.delimiterEnd !== false ||
+        columnIndex !== mostCellsPerRow - 1
+      ) {
+        line.push('|')
+      }
+    }
+
+    lines.push(
+      settings.delimiterEnd === false
+        ? line.join('').replace(/ +$/, '')
+        : line.join('')
+    )
+  }
+
+  return lines.join('\n')
+}
+
+/**
+ * @param {string|null|undefined} [value]
+ * @returns {string}
+ */
+function serialize(value) {
+  return value === null || value === undefined ? '' : String(value)
+}
+
+/**
+ * @param {string} value
+ * @returns {number}
+ */
+function defaultStringLength(value) {
+  return value.length
+}
+
+/**
+ * @param {string} value
+ * @returns {number}
+ */
+function toAlignment(value) {
+  var code = typeof value === 'string' ? value.charCodeAt(0) : 0
+
+  return code === 67 /* `C` */ || code === 99 /* `c` */
+    ? 99 /* `c` */
+    : code === 76 /* `L` */ || code === 108 /* `l` */
+    ? 108 /* `l` */
+    : code === 82 /* `R` */ || code === 114 /* `r` */
+    ? 114 /* `r` */
+    : 0
+}
+
+
+/***/ }),
+
 /***/ 467:
 /***/ ((module, exports, __nccwpck_require__) => {
 
@@ -5676,6 +5957,84 @@ function onceStrict (fn) {
 
 /***/ }),
 
+/***/ 976:
+/***/ ((module) => {
+
+"use strict";
+/*!
+ * repeat-string <https://github.com/jonschlinkert/repeat-string>
+ *
+ * Copyright (c) 2014-2015, Jon Schlinkert.
+ * Licensed under the MIT License.
+ */
+
+
+
+/**
+ * Results cache
+ */
+
+var res = '';
+var cache;
+
+/**
+ * Expose `repeat`
+ */
+
+module.exports = repeat;
+
+/**
+ * Repeat the given `string` the specified `number`
+ * of times.
+ *
+ * **Example:**
+ *
+ * ```js
+ * var repeat = require('repeat-string');
+ * repeat('A', 5);
+ * //=> AAAAA
+ * ```
+ *
+ * @param {String} `string` The string to repeat
+ * @param {Number} `number` The number of times to repeat the string
+ * @return {String} Repeated string
+ * @api public
+ */
+
+function repeat(str, num) {
+  if (typeof str !== 'string') {
+    throw new TypeError('expected a string');
+  }
+
+  // cover common, quick use cases
+  if (num === 1) return str;
+  if (num === 2) return str + str;
+
+  var max = str.length * num;
+  if (cache !== str || typeof cache === 'undefined') {
+    cache = str;
+    res = '';
+  } else if (res.length >= max) {
+    return res.substr(0, max);
+  }
+
+  while (max > res.length && num > 1) {
+    if (num & 1) {
+      res += str;
+    }
+
+    num >>= 1;
+    str += str;
+  }
+
+  res += str;
+  res = res.substr(0, max);
+  return res;
+}
+
+
+/***/ }),
+
 /***/ 294:
 /***/ ((module, __unused_webpack_exports, __nccwpck_require__) => {
 
@@ -6167,6 +6526,34 @@ module.exports = require("zlib");;
 /******/ 	}
 /******/ 	
 /************************************************************************/
+/******/ 	/* webpack/runtime/define property getters */
+/******/ 	(() => {
+/******/ 		// define getter functions for harmony exports
+/******/ 		__nccwpck_require__.d = (exports, definition) => {
+/******/ 			for(var key in definition) {
+/******/ 				if(__nccwpck_require__.o(definition, key) && !__nccwpck_require__.o(exports, key)) {
+/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
+/******/ 				}
+/******/ 			}
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
+/******/ 	(() => {
+/******/ 		__nccwpck_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	(() => {
+/******/ 		// define __esModule on exports
+/******/ 		__nccwpck_require__.r = (exports) => {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	})();
+/******/ 	
 /******/ 	/* webpack/runtime/compat */
 /******/ 	
 /******/ 	if (typeof __nccwpck_require__ !== 'undefined') __nccwpck_require__.ab = __dirname + "/";/************************************************************************/
--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import * as path from 'path'
 import * as fs from 'fs'
 import * as core from '@actions/core'
 import * as github from '@actions/github'
+import { markdownTable } from 'markdown-table'
 import comment from './comment'
 
 const WORKSPACE: string = process.env.GITHUB_WORKSPACE!
@@ -18,6 +19,16 @@ function parseLastRun(filePath: string): LastRun {
   return JSON.parse(content.toString()) as LastRun
 }
 
+function summaryTable(): string {
+  const baseCoveredPercent = parseLastRun('base-coverage-reports/.last_run.json')
+  const headCoveredPercent = parseLastRun('head-coverage-reports/.last_run.json')
+
+  return markdownTable([
+    ['Base Coverage', 'Head Coverage'],
+    [`${baseCoveredPercent.result.line}%`, `${headCoveredPercent.result.line}%`]
+  ])
+}
+
 async function run(): Promise<void> {
   try {
     const pullRequestId = github.context.issue.number
@@ -26,9 +37,7 @@ async function run(): Promise<void> {
       return
     }
 
-    const coveredPercent = parseLastRun('head-coverage-reports/.last_run.json')
-
-    await comment(pullRequestId, `Test coverage percent: ${coveredPercent}`)
+    await comment(pullRequestId, summaryTable())
   }
   catch (error) {
     core.setFailed(error.message)
